fb8f4da6edd3080dacd8f93f8a3099e431fcef1f
Anwar3006 <anwarsadat.d2@gmail.com>
2023-05-29 19:32:25 +0000

    Starwars api

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,34 @@
+const request = require('request');
+
+const baseUrl = 'https://swapi.dev/api/films/';
+
+// Function to fetch the characters of a Star Wars movie
+const fetchCharacters = (movieID) => {
+  const fullUrl = baseUrl + movieID;
+  request(fullUrl, (error, response, body) => {
+    if (error | response.statusCode !== 200) {
+      console.log('Error with request: ', error);
+    }
+
+    const convertToJson = JSON.parse(body);
+    const characterList = convertToJson.characters;
+
+    characterList.forEach(characterUrl => {
+      request(characterUrl, (error, response, body) => {
+        if (error | response.statusCode !== 200) {
+          console.log('Error with request: ', error);
+        }
+
+        const parseToJson = JSON.parse(body);
+        const getName = parseToJson.name;
+        console.log(getName);
+      });
+    });
+  });
+};
+
+// Get Id from Commandline
+const getID = process.argv[2];
+
+// pass to function
+fetchCharacters(getID);
