eecfb06905024972766385369c5668bd079f9b88
Anwar3006 <anwarsadat.d2@gmail.com>
2023-06-16 17:40:00 +0000

    coin change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -3,15 +3,17 @@
 Coin Change Problem.
 """
 
+
 def makeChange(coins, total):
     """
     Given a pile of coins of different values,
     determine the fewest number of coins needed to meet a given amount total.
     """
     amount = total
-    if total == 0:
+    if total <= 0:
         return 0
-    # create list to house the number of coins needed for each amount from 1 to the amount
+    # create list to house the number of coins needed for each amount
+    # from 1 to the amount
     DP_amount = [float('inf') for _ in range(amount + 1)]
     # set 0th index to 0(0 requires 0 coins)
     DP_amount[0] = 0
